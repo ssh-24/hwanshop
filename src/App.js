c7e2588510722e19f2e6e5b31a67521d41e10078
Hwan <65848753+ssh-24@users.noreply.github.com>
2023-02-11 23:32:44 +0900

    230211 23:32

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 /*eslint-disable*/
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
