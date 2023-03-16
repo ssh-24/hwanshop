0edef12c4262daacdf92b7c877a0d5fb557434b7
Hwan <65848753+ssh-24@users.noreply.github.com>
2023-03-17 01:19:01 +0900

    230317 01:18 이미지 정리

--- src/App.js
+++ src/App.js
@@ -16,42 +16,26 @@ function App() {
         </Container>
       </Navbar>
 
-      {/* 메인사진 */}
+      {/* 배너 이미지 */}
       <div className="main-bg" style={{backgroundImage : 'url('+bgImg+')'}}></div>
+      {/* 원본링크로 삽입하는 방식 */}
+      {/* <div className="main-bg" style={{backgroundImage : 'url("https://img.freepik.com/free-vector/banner-with-a-pair-of-realistic-black-sneakers-with-shadow-and-text_548887-94.jpg?w=1800&t=st=1678982266~exp=1678982866~hmac=bdda604f4a26be8ee73498fa077981fa7187483502bdf6a0a9dac0ecdffd97a2")'}}></div> */}
 
       {/* 상품 */}
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + '/shoes/nike.jpg'} width='85%' height='70%'/>
+            <img src={process.env.PUBLIC_URL + '/shoes/product1.png'} width='85%' height='70%'/>
             <h4>상품명</h4>
             <p>상품설명</p>
           </div>
           <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + '/shoes/nb992.jpg'} width='85%' height='70%'/>
+            <img src={process.env.PUBLIC_URL + '/shoes/product2.png'} width='85%' height='70%'/>
             <h4>상품명</h4>
             <p>상품설명</p>
           </div>
           <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + '/shoes/vans.jpg'} width='85%' height='70%'/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + '/shoes/nb530.jpg'} width='85%' height='70%'/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + '/shoes/mmds.jpg'} width='85%' height='70%'/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + '/shoes/converse.jpg'} width='85%' height='70%'/>
+            <img src={process.env.PUBLIC_URL + '/shoes/product3.png'} width='85%' height='70%'/>
             <h4>상품명</h4>
             <p>상품설명</p>
           </div>
@@ -59,17 +43,53 @@ function App() {
 
         <div className="row">
           <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + '/shoes/adidas.jpg'} width='85%' height='70%'/>
+            <img src={process.env.PUBLIC_URL + '/shoes/product4.png'} width='85%' height='70%'/>
             <h4>상품명</h4>
             <p>상품설명</p>
           </div>
           <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + '/shoes/nike.jpg'} width='85%' height='70%'/>
+            <img src={process.env.PUBLIC_URL + '/shoes/product5.png'} width='85%' height='70%'/>
             <h4>상품명</h4>
             <p>상품설명</p>
           </div>
           <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + '/shoes/nike.jpg'} width='85%' height='70%'/>
+            <img src={process.env.PUBLIC_URL + '/shoes/product6.png'} width='85%' height='70%'/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + '/shoes/product7.png'} width='85%' height='70%'/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + '/shoes/product8.png'} width='85%' height='70%'/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + '/shoes/product9.png'} width='85%' height='70%'/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + '/shoes/product10.png'} width='85%' height='70%'/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + '/shoes/product11.png'} width='85%' height='70%'/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src={process.env.PUBLIC_URL + '/shoes/product12.png'} width='85%' height='70%'/>
             <h4>상품명</h4>
             <p>상품설명</p>
           </div>
