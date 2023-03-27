54e890bda1073eec482f6d3f83beca0ab12110f9
ssh-24 <sonsh5364@gmail.com>
2023-03-27 17:02:44 +0900

    중괄호 있고 없고가 뭔 상관이지

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { React } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -9,13 +9,13 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
